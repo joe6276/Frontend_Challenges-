43757b462b270d281a5c64a2a01c511b131192b3
Joe6276 <joendambuki16@gmail.com>
2022-01-06 09:31:15 +0300

    added a product page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Card from "./Component/Card";
 import Homepage from "./Component/Homepage";
+import ProductPage from "./Component/ProductPage";
 
+import './app.css'
 function App() {
   return (
-    <div className="App">
-     
-<Homepage/>
-<Card/>
+    <div className="app-container">
+
+      {/* <Homepage/>
+<Card/> */}
+      <ProductPage />
     </div>
   );
 }
--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -6,7 +6,7 @@ import data from '../data.json'
 export default class Card extends Component {
     render() {
         return (
-        <div className='d-flex'> 
+        <div  style={{display:'flex', flexDirection:'column'}}> 
             {data.challenges.map((challenge, i) =>(
             <Cards key={i} index={i+1} challenge={challenge}/>
             ))}
