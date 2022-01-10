1060ce7c4bf4116059a12f0ed367d7db40122513
Joe6276 <joendambuki16@gmail.com>
2022-01-10 14:42:50 +0300

    started working on rooms

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Testimonial from "./Component/Testimonial";
 
 import './app.css'
 import { Route, Routes } from "react-router";
+import Room from "./Component/Room";
 function App() {
   return (
     <div className="app-container">
@@ -14,6 +15,7 @@ function App() {
         
         <Route path="/challenge/first" element={<ProductPage/>}></Route>
         <Route path="/challenge/second" element={<Testimonial/>}></Route>
+        <Route path="/challenge/third" element= {<Room/>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Component/Room.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Carosel from './carousel/Carousel'
+export default class Room extends Component {
+    render() {
+        return (
+            <div>
+               <Carosel/>
+            </div>
+        )
+    }
+}
